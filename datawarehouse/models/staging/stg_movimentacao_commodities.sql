90f8c841d3cb44c2b7d54c8f426e1c582f2aede5
VitorJustiniano <vitor.mjustiniano@hotmail.com>
2025-03-05 23:51:45 -0300

    adicionar datamart e staging

---
+++ datawarehouse/models/staging/stg_movimentacao_commodities.sql
@@ -0,0 +1,27 @@
+with source as (
+    select
+        date,
+        symbol,
+        action,
+        quantity
+    from
+        {{ source('dbsales_fbv3', 'movimentacao_commodities') }}
+),
+
+
+renamed as (
+    select
+        cast(date as date) as data,
+        symbol as simbolo,
+        action as acao,
+        quantity as quantidade
+    from source
+)
+
+
+select 
+    data,
+    simbolo,
+    acao,
+    quantidade
+from renamed
